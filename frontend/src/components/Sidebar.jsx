7ade38754eae14e88e95ada3ae8fc86be001f6fe
Avupati Ganesh <23pa5a0501@vishnu.edu.in>
2025-08-15 12:16:59 +0530

    sidebar

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+
 import './Sidebar.css';
 
 const itemsMap = {
@@ -6,14 +7,18 @@ const itemsMap = {
   Kids: ['Shirts', 'Pants', 'Belt'],
 };
 
-const Sidebar = ({ selectedCategory, onItemSelect }) => (
-  <aside className="sidebar">
-    <ul>
-      {itemsMap[selectedCategory].map(item => (
-        <li key={item} onClick={() => onItemSelect(item)}>{item}</li>
-      ))}
-    </ul>
-  </aside>
-);
+
+const Sidebar = ({ selectedCategory, onItemSelect, visible }) => {
+  if (!visible) return null;
+  return (
+    <aside className="sidebar">
+      <ul>
+        {itemsMap[selectedCategory].map(item => (
+          <li key={item} onClick={() => onItemSelect(item)}>{item}</li>
+        ))}
+      </ul>
+    </aside>
+  );
+};
 
 export default Sidebar;
